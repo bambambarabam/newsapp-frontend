e82a932dd5848b728db9cf55a2301e0ecafb765b
Slava Lebedintsev <xcarvedx@yandex.ru>
2020-12-04 22:49:02 +0300

    feat: функциональность infoToolTip

--- src/components/InfoTooltip/InfoTooltip.js
+++ src/components/InfoTooltip/InfoTooltip.js
@@ -2,12 +2,13 @@ import React from 'react';
 import PopupWithForm from '../PopupWithForm/PopupWithForm';
 import './InfoTooltip.css';
 
-function InfoTooltip({ isOpen, onClose }) {
+function InfoTooltip({ isOpen, onClose, onChangeForm }) {
   return (
     <PopupWithForm
       formName='tooltip'
       isOpen={isOpen}
-      onClose={onClose}>
+      onClose={onClose}
+      onChangeForm={onChangeForm}>
       <legend className='popup__heading'>Пользователь успешно зарегистрирован!</legend>
     </PopupWithForm>
   );
